4c368d3f58caf608952ec0402fb529b244b3af11
priyanshujn <jainpriyanshu709@gmail.com>
2023-09-09 18:10:56 +0530

    updated appointment schema

--- models/appointmentModel.js
+++ models/appointmentModel.js
@@ -9,10 +9,6 @@ const appointmentSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    date: {
-        type: String,
-        required: true
-    },
     startTime: {
         type: String,
         required: true
